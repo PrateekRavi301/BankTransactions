223d99ea3a2856af538b4de03d08e7adc09fa7bd
Prateek <prateek38gautam@gmail.com>
2024-02-22 20:10:08 +0530

    Add project files.

---
+++ BankTransactions/Migrations/20240222081008_Intitial Create.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankTransactions.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    BeneficiaryNumber = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    SwiftCode = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    Ammount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ BankTransactions/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransactions.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName ="nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(12,ErrorMessage ="Maximum 12 characters only.")]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Beneficiary Number")]
+        [Required(ErrorMessage ="This field is required.")]
+        public string BeneficiaryNumber { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Swift Code")]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public string SwiftCode {  get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string Ammount { get; set;}
+        [DisplayFormat(DataFormatString ="{0:MMM-dd-yy}")]
+        public DateTime Date {  get; set; }
+    }
+}
